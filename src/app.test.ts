a46f3b26b915cbf4453c649a4abab94a4d3ee973
AldrinP <aldrin.pernito@gmail.com>
2022-02-07 21:59:45 +0000

    Finish robot should rotate right in a full rotation and refactor execute method by creating separate method rotateRight

--- src/app.test.ts
+++ src/app.test.ts
@@ -4,7 +4,10 @@ describe('Martina Robot should ...', () => {
 
   describe.each([
     ['0 0 R', '0 0 N', '0 0 E'],
-  ])('Robot should rotate right', (commands, initialPosition, currentPosition) => {
+    ['0 0 RR', '0 0 N', '0 0 S'],
+    ['0 0 RRR', '0 0 N', '0 0 W'],
+    ['0 0 RRRR', '0 0 N', '0 0 N'],
+  ])('rotate right', (commands, initialPosition, currentPosition) => {
     const robot = new Robot(initialPosition);
 
     test(`returns ${currentPosition} when ${commands}`, () => {
--- src/robot.ts
+++ src/robot.ts
@@ -9,11 +9,23 @@ export class Robot {
     const commandsLength = commands.length;
 
     for(let i=0; i < commandsLength; i++) {
-      if (this.direction === 'N') {
-        this.direction = 'E';
+      if (commands.charAt(i) === 'R') {
+        this.direction = this.rotateRight();
       }
     }
     return `0 0 ${this.direction}`;
+  }
+
+  rotateRight(): string {
+    if (this.direction === 'S') {
+      return 'W';
+    } else if (this.direction === 'E') {
+      return 'S';
+    } else if (this.direction === 'N') {
+      return 'E';
+    } else {
+      return 'N';
+    }
   }
 
 }
